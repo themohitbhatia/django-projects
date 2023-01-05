15b1e9f53a89d312c1be6e82bc41d54f5469e351
Mohit Bhatia <mohitbhatia23072001@gmail.com>
2023-01-06 00:59:33 +0530

    register login logout accountUpdate

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -9,6 +9,7 @@ def home_screen_view(request):
     accounts = Account.objects.all()
     context['accounts'] = accounts
     
+    return render(request, "personal/home.html", context)
     
     
     
@@ -30,5 +31,3 @@ def home_screen_view(request):
 
     # questions = Question.objects.all()
     # context['questions'] = questions
-
-    # return render(request, "personal/home.html", context)
