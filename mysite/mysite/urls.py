59111fe0eb6482e5f9f891990aee811cf8161b10
Mohit Bhatia <mohitbhatia23072001@gmail.com>
2023-01-09 01:22:41 +0530

    starting with rest

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -41,6 +41,10 @@ urlpatterns = [
     path("must_authenticate_view/", must_authenticate_view, name="must_authenticate"),
     path("register/", registration_view, name="register"),
 
+    # PEST FRAMEWORK URLS
+    path('api/blog/', include('blog.api.urls', 'blog_api')),
+    # path('api/account/', include('account.api.urls', 'account_api')),
+
     # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
         name='password_change_done'),
